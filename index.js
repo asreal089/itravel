2bf72ef5fdbd4e38d4767fd2c33188ab30ddc193
asreal089 <lexx_89@hotmail.com>
2020-06-19 10:33:20 -0300

    Add: google oauth

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 const app = express();
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClienteID,
+			clientSecret: keys.googleClienteSecret,
+			callbackURL: '/auth/callback',
+		},
+		(accessToken) => {
+			console.log(accessToken);
+		}
+	)
+);
 
 app.get('/', (req, res) => {
 	res.send({ olarr: 'mundo' });
 });
+
+app.get(
+	'/auth',
+	passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 8585;
 app.listen(PORT);
