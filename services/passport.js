de0d3ef5bec6155c44bf0d7c306f249011794ed1
asreal089 <lexx_89@hotmail.com>
2020-06-20 18:23:01 -0300

    refactor de promessas

--- services/passport.js
+++ services/passport.js
@@ -23,20 +23,17 @@ passport.use(
 			callbackURL: '/auth/callback',
 			proxy: true,
 		},
-		(accessToken, refreshToken, profile, done) => {
-			User.findOne({ googleId: profile.id }).then((existingUser) => {
-				if (existingUser) {
-					done(null, existingUser);
-				} else {
-					new User({
-						googleId: profile.id,
-						nome: profile._json.name,
-						email: profile._json.email,
-					})
-						.save()
-						.then((user) => done(null, user));
-				}
+		async (accessToken, refreshToken, profile, done) => {
+			const existingUser = await User.findOne({ googleId: profile.id });
+			if (existingUser) {
+				return done(null, existingUser);
+			}
+			const user = await new User({
+				googleId: profile.id,
+				nome: profile._json.name,
+				email: profile._json.email,
 			});
+			return done(null, user);
 		}
 	)
 );
