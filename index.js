b78d63bbc1a0cdc63c13452dc0c41e4989b1e330
asreal089 <lexx_89@hotmail.com>
2020-06-21 20:24:40 -0300

    Fix rotas prod

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ moongose.connect(keys.mongoURI, {
 	useUnifiedTopology: true,
 });
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 const PORT = process.env.PORT || 8585;
 app.listen(PORT);
 console.log('aplicação de pé na porta: ' + PORT);
