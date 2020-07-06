78bc5b21b2c5ee09f4d6871031a91e58cc43548d
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-07-05 21:20:31 -0300

    aiaiai

--- client/src/components/TravelSearches.js
+++ client/src/components/TravelSearches.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
+import { Row } from 'react-materialize';
+import { Col } from 'react-materialize';
+import { Card } from 'react-materialize';
+import { Table } from 'react-materialize';
+import { IoIosAddCircleOutline } from 'react-icons/io';
 
 const axios = require('axios');
 
@@ -27,7 +32,7 @@ class TravelSearches extends Component {
                 <p>Data de Ida: {this.props.location.state.data_ida}</p>
                 <p>Data de Volta: {this.props.location.state.data_volta}</p>
 
-                
+
                 <h2>Passagens Aéreas:</h2>
                 <p>{JSON.stringify(this.props.location.state.flights)}</p>
                 <h2>Hospedagem:</h2>
@@ -36,6 +41,32 @@ class TravelSearches extends Component {
                 {console.log("tipo desse caray:" + typeof this.props.location.state.flights )}
                 {console.log("tipo desse caray2: " + typeof this.props.location.state.hotels )}
 
+					<Row>
+						<Col m={6} s={12}>
+							<Card
+								// className="purple"
+								// textClassName="white-text"
+								// title={this.props.location.state.hotels.term}
+							>
+								<Table>
+									 <tbody>
+										{this.props.location.state.hotel.suggestions.map((entidade) => (
+											<tr>
+												<td>{entidade.type}</td>
+												<td>{entidade.name}</td>
+												<td>
+													<IoIosAddCircleOutline
+														size={22}
+														color="#EEE"
+													/>
+												</td>
+											</tr>
+										))}
+									</tbody> 
+								</Table>
+							</Card>
+						</Col>
+					</Row>
 			</div>
 		);
 	}
