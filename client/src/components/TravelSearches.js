c418c0810eeb9bf59a8e9363d11190dcf88a156c
jessmelo <50722304+jessmelo@users.noreply.github.com>
2020-07-05 21:09:34 -0300

    edit

--- client/src/components/TravelSearches.js
+++ client/src/components/TravelSearches.js
@@ -22,16 +22,17 @@ class TravelSearches extends Component {
 
             <div>
 				<h1>Resultados da pesquisa:</h1>
-                <p>{this.props.location.state.cidade_origem}</p>
-                <p>{this.props.location.state.cidade_destino}</p>
-                <p>{this.props.location.state.data_ida}</p>
-                <p>{this.props.location.state.data_volta}</p>
-                <p>{this.props.location.state.teste}</p>
+                <p>Cidade de Origem: {this.props.location.state.cidade_origem}</p>
+                <p>Destino: {this.props.location.state.cidade_destino}</p>
+                <p>Data de Ida: {this.props.location.state.data_ida}</p>
+                <p>Data de Volta: {this.props.location.state.data_volta}</p>
+
+                
                 <h2>Passagens Aéreas:</h2>
                 <p>{JSON.stringify(this.props.location.state.flights)}</p>
                 <h2>Hospedagem:</h2>
                 <p>{JSON.stringify(this.props.location.state.hotels)}</p>
-                
+
                 {console.log("tipo desse caray:" + typeof this.props.location.state.flights )}
                 {console.log("tipo desse caray2: " + typeof this.props.location.state.hotels )}
 
